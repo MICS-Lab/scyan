cb06f3a572b3f84709c43335d4d5103283d906be
Blampey Quentin <quentin.blampey@student.ecp.fr>
2022-02-21 16:54:03 +0100

    plot matrix_pop_reconstruction

---
+++ scyan/plot.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import seaborn as sns
+
+from .model import Scyan
+
+def matrix_pop_reconstruction(scyan: Scyan):
+    predictions = scyan.predict(key_added=None)
+    predictions.name = "population"
+    h = scyan.module(scyan.x)[0].detach().numpy()
+    df = pd.concat([predictions, pd.DataFrame(h, columns=scyan.marker_pop_matrix.index)], axis=1)
+    df = df.groupby("population").median()
+    sns.heatmap(df.loc[scyan.marker_pop_matrix.columns] - scyan.marker_pop_matrix.T, cmap="coolwarm")
