9aeb92dd9fa3d7fa8d10cb1ae55ad9582f24aaec
Blampey Quentin <quentin.blampey@student.ecp.fr>
2022-08-31 18:28:24 +0200

    fix plots colors

--- scyan/__init__.py
+++ scyan/__init__.py
@@ -1,14 +1,15 @@
-import logging
 import importlib.metadata
+import logging
 
 import pytorch_lightning as pl
-
-__version__ = importlib.metadata.version("scyan")
-
-logging.basicConfig(level=logging.INFO)
+import scanpy as sc
 
 from .model import Scyan
 from . import data, plot, tools, utils
 from .utils import read_fcs, write_fcs
 
+__version__ = importlib.metadata.version("scyan")
+
+logging.basicConfig(level=logging.INFO)
+sc.set_figure_params(facecolor="white", fontsize=10)
 pl.seed_everything(0)
