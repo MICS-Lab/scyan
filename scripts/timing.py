4011b548cdd87eef41eb79241340d519d504b2ce
Blampey Quentin <quentin.blampey@student.ecp.fr>
2023-01-11 17:05:15 +0100

    timing script: support n_obs argument

--- scripts/timing.py
+++ scripts/timing.py
@@ -13,17 +13,13 @@ from . import utils
 @hydra.main(version_base=None, config_path="../config", config_name="config")
 def main(config: DictConfig) -> None:
     """Run scyan on a dataset specified by the config/config.yaml with different number of cells.
-    NB: the only purpose of this file is to time the model on AML.
+    NB: the only purpose of this file is to time the model on POISED.
 
     Args:
         config: Hydra generated configuration (automatic).
-
-    Returns:
-        Metric chosen by the config to be optimized for hyperparameter search, e.g. the loss.
     """
     pl.seed_everything(config.seed)
 
-    ### Instantiate everything
     adata, table = scyan.data.load(config.project.name)
     n_obs = config.project.get("n_obs", None)
 
