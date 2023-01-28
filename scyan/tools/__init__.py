a5c2794b757a34980ee1c40c6522898e004caf88
Blampey Quentin <quentin.blampey@student.ecp.fr>
2023-01-28 13:06:22 +0100

    new get palette function

--- scyan/tools/__init__.py
+++ scyan/tools/__init__.py
@@ -1,3 +1,4 @@
 from .representation import umap, subcluster
 from .result_analysis import count_cell_populations, mean_intensities
 from .gating import PolygonGatingUMAP
+from .colors import palette_level
