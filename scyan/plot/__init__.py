93235e5a8d533a46e538f465afb7554a35148452
Blampey Quentin <quentin.blampey@centralesupelec.fr>
2023-06-28 14:34:27 +0200

    added plots: population perc and dynamics

--- scyan/plot/__init__.py
+++ scyan/plot/__init__.py
@@ -3,4 +3,5 @@ from .heatmap import probs_per_marker
 from .dot import scatter, umap, pop_level
 from .graph import pops_hierarchy
 from .expressions import pop_expressions, pops_expressions, boxplot_expressions
+from .ratios import pop_percentage, pop_dynamics
 from ._scanpy_plot import reset_plot_settings
